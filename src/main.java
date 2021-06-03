1471e3e5e5a93766867b16b44472897edc2176aa
Woxsart <45289602+Woxsart@users.noreply.github.com>
2021-06-03 16:16:38 +0200

    create accueil

--- src/main.java
+++ src/main.java
@@ -5,6 +5,50 @@ import java.util.*;
 public class main {
 	public static void main(String[] args) throws IOException {
 
+		//staff vers temp
+
+		FileReader lecteur;
+		FileWriter ecrivain;
+
+		lecteur = new FileReader("ressource/staff.txt");
+		ecrivain = new FileWriter("ressource/temp.txt");
+
+		BufferedReader reader1 = new BufferedReader(lecteur);
+		String s;
+		while((s = reader1.readLine()) != null) {
+			ecrivain.write("<a href='"+"'>" + s + "</a><br>" + "\n");
+		}
+		reader1.close();
+		lecteur.close();
+		ecrivain.close();
+
+		//supp de accueil.html
+
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("src/accueil.html", false)));
+		pw.println("");
+		pw.close();
+
+		//temp vers accueil.html
+		int c;
+
+		lecteur = new FileReader("ressource/temp.txt");
+		ecrivain = new FileWriter("src/accueil.html");
+
+		ecrivain.write("<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta charset='utf-8' />\n\t\t<title>Accueil</title>\n\t</head>\n\t<body>\n"); //ecrire le début du fichier html
+		while((c = lecteur.read()) != -1) {
+			ecrivain.write(c);
+		}
+		ecrivain.write("\n\t</body>\n</html>"); //ecrire la fin du fichier html
+		lecteur.close();
+		ecrivain.close();
+
+		//del temp file
+		pw = new PrintWriter(new BufferedWriter(new FileWriter("ressource/temp.txt", false)));
+		pw.println("");
+		pw.close();
+
+		//__________________________________________________________________
+/*
 		//test vers temp
 		FileReader lecteur;
 		FileWriter ecrivain;
@@ -16,11 +60,11 @@ public class main {
 		String s;
 		String[] tab;
 		while((s = reader.readLine()) != null) {
-			/*
+
 			tab = s.split("\t");
 			List<String> res = Arrays.asList(tab);
 			System.out.println(res);
-			*/
+
 			ecrivain.write(s + "<br>" + "\n");
 		}
 		reader.close();
@@ -49,6 +93,8 @@ public class main {
 		pw = new PrintWriter(new BufferedWriter(new FileWriter("ressource/temp.txt", false)));
 		pw.println("");
 		pw.close();
+
+ */
 	}
 
 }
