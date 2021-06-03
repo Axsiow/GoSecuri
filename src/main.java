0e2fad02fbee2e073893d3d7a5ff0e19ee5da144
ShrekZiBest <45289696+LeBoJeu@users.noreply.github.com>
2021-06-03 14:42:25 +0200

    Fiche agent / liste materiel

--- src/main.java
+++ src/main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class main {
 	public static void main(String[] args) throws IOException {
-		
+
 		//test vers temp
 		FileReader lecteur;
 		FileWriter ecrivain;
