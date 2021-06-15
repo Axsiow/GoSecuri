02536da172f5d81fbe7fc582e907b6c2260a525b
Woxsart <45289602+Woxsart@users.noreply.github.com>
2021-06-15 16:52:04 +0200

    Update main.java

--- src/main.java
+++ src/main.java
@@ -26,7 +26,6 @@ public class main {
 		ecrivain.close();
 
 		//supp de accueil.html
-
 		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("ressource/web/accueil.html", false)));
 		pw.println("");
 		pw.close();
@@ -50,56 +49,8 @@ public class main {
 		pw.println("");
 		pw.close();
 
-		//__________________________________________________________________
-/*
-		//test vers temp
-		FileReader lecteur;
-		FileWriter ecrivain;
-
-		lecteur = new FileReader("ressource/test.txt");
-		ecrivain = new FileWriter("ressource/temp.txt");
-
-		BufferedReader reader = new BufferedReader(lecteur);
-		String s;
-		String[] tab;
-		while((s = reader.readLine()) != null) {
-
-			tab = s.split("\t");
-			List<String> res = Arrays.asList(tab);
-			System.out.println(res);
-
-			ecrivain.write(s + "<br>" + "\n");
-		}
-		reader.close();
-		lecteur.close();
-		ecrivain.close();
-
-		//supp de html
-		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("src/test_copie.html", false)));
-		pw.println("");
-		pw.close();
-
-		//temp vers html
-		int c;
-
-		lecteur = new FileReader("ressource/temp.txt");
-		ecrivain = new FileWriter("src/test_copie.html");
-		ecrivain.write("<!DOCTYPE html>\n<html>\n\t<head>\n\t\t<meta charset='utf-8' />\n\t\t<title>Titre</title>\n\t</head>\n\t<body>\n"); //ecrire le début du fichier html
-		while((c = lecteur.read()) != -1) {
-			ecrivain.write(c);
-		}
-		ecrivain.write("\n\t</body>\n</html>"); //ecrire la fin du fichier html
-		lecteur.close();
-		ecrivain.close();
-
-		//del temp file
-		pw = new PrintWriter(new BufferedWriter(new FileWriter("ressource/temp.txt", false)));
-		pw.println("");
-		pw.close();
-
- */
-
 	}
+	
 	public static void createStaffHtml(String page){
 		try {
 			FileReader lecteur1 = new FileReader("ressource/txt/"+page+".txt");
@@ -127,12 +78,7 @@ public class main {
 			{
 				listeHtml.put(str.split("\t")[0], str.split("\t")[1]);
 			}
-/*
-			for(String test : yo){
-				paul++;
-				System.out.println(paul);
-				new BufferedReader(lecteur2).readLine();
-			}*/
+
 			count = 0;
             ArrayList outils = new ArrayList();
 
@@ -167,8 +113,6 @@ public class main {
                     }
 
 			}
-
-			//new BufferedReader(lecteur)
 
 			ecrivain.write("\n\t</body>\n</html>"); //ecrire la fin du fichier html
 
